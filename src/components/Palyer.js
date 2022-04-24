e6c610f38146e8ace2bcc3f0ebbeeffb80ae0489
Gilles Elan <gilles.muess@elan-formation.fr>
2022-04-24 19:32:51 +0200

    Updated player section

--- src/components/Palyer.js
+++ src/components/Palyer.js
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faPlay,
+  faPause,
   faAngleLeft,
   faAngleRight,
 } from '@fortawesome/free-solid-svg-icons';
@@ -19,20 +20,46 @@ function Palyer({ currentSong, isPlaying, setIsPlaying }) {
       setIsPlaying(!isPlaying);
     }
   };
+  const timeUpdateHandler = (e) => {
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime, duration });
+  };
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2)
+    );
+  };
+  const dragHandler = (e) => {
+    const currentTime = e.target.value;
+    audioRef.current.currentTime = currentTime;
+    setSongInfo({ ...songInfo, currentTime });
+  };
+  //State
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
 
   return (
     <div className='player'>
       <div className='time-control'>
-        <p>Start Time</p>
-        <input type='range' />
-        <p>End Time</p>
+        <p>{getTime(songInfo.currentTime)}</p>
+        <input
+          min={0}
+          max={songInfo.duration}
+          value={songInfo.currentTime}
+          onChange={dragHandler}
+          type='range'
+        />
+        <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className='play-control'>
         <FontAwesomeIcon className='skip-back' icon={faAngleLeft} size='2x' />
         <FontAwesomeIcon
           onClick={playSongHandler}
           className='play'
-          icon={faPlay}
+          icon={isPlaying ? faPause : faPlay}
           size='2x'
         />
         <FontAwesomeIcon
@@ -41,7 +68,12 @@ function Palyer({ currentSong, isPlaying, setIsPlaying }) {
           size='2x'
         />
       </div>
-      <audio ref={audioRef} src={currentSong.audio}></audio>
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 }
