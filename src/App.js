081bc71fa6b79e8f107aec8e0ba8b88503dcb9ed
Gilles Elan <gilles.muess@elan-formation.fr>
2022-04-25 22:07:02 +0200

    Input time styled

--- src/App.js
+++ src/App.js
@@ -19,13 +19,19 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
   // Handlers
   const timeUpdateHandler = (e) => {
     const currentTime = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime, duration });
+    const roundedCurrentTime = Math.round(currentTime);
+    const roundedDuration = Math.round(duration);
+    const animationPercentage = Math.round(
+      (roundedCurrentTime / roundedDuration) * 100
+    );
+    setSongInfo({ ...songInfo, currentTime, duration, animationPercentage });
   };
 
   return (
